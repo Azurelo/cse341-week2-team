34f926add49858cf97d3eb8fc6dea8f3f95494c2
Justus <galaxycheesemc@gmail.com>
2025-01-18 11:36:17 -0700

    database connection and get requests

---
+++ controllers/contactsController.js
@@ -0,0 +1,24 @@
+const Contact = require('../models/Contact');
+
+// Get all contacts
+exports.getAllContacts = async (req, res) => {
+    try {
+        const contacts = await Contact.find();
+        res.json(contacts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Get a single contact by ID
+exports.getContactById = async (req, res) => {
+    try {
+        const contact = await Contact.findById(req.params.id);
+        if (!contact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json(contact);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
